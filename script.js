50cf9c1511a77d1269dbda9d658db2591986e99b
Alif Akbar <alifakbartroll@gmail.com>
2025-01-21 21:23:21 +0700

    tests

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
- 
+document.querySelectorAll('.post').forEach(card => {
+    card.addEventListener('click', () => {
+        const link = card.getAttribute('data-href');
+        if (link) {
+            window.location.href = link;
+        }
+    });
+});
